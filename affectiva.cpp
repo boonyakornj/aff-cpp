6a5494eaf4302685c2817e403673e5c99d8fd13d
Boonyakorn Jantaranuson <b.jantaranuson@gmail.com>
2017-10-12 00:25:01 +0200

    Added listeners to file. Process video and output to csv file (still no output)

--- affectiva.cpp
+++ affectiva.cpp
@@ -1,8 +1,13 @@
 // Need to run on g++ version not greater than 4.8
 
 #include <fstream>
+#include <iostream>
 #include "VideoDetector.h"
 #include <boost/filesystem.hpp>
+
+#include "AFaceListener.hpp"
+#include "PlottingImageListener.hpp"
+#include "StatusListener.hpp"
 
 using namespace affdex;
 using namespace std;
@@ -14,13 +19,15 @@ int main(int argc, char ** argsv)
   const unsigned int maxNumFaces = 1;
   const FaceDetectorMode faceConfig = FaceDetectorMode::LARGE_FACES;
   string videoPath = "59c13748145ae01ec3675d3b.mp4";
+  bool drawDisplay = false;
 
   // Create a video detector to analyse the video
-  VideoDetector detector(processFrameRate, maxNumFaces, faceConfig);  // using default value because of `undefined reference error`
+  shared_ptr<VideoDetector> detector;
+  detector = std::make_shared<VideoDetector>(processFrameRate, maxNumFaces, faceConfig);
 
   // Configure the detector
   string classifierPath = "/home/boonyakorn/affdex-sdk/data";
-  detector.setClassifierPath(classifierPath);
+  detector->setClassifierPath(classifierPath);
 
   // Initialize output file
   boost::filesystem::path csvPath(videoPath);
@@ -29,21 +36,45 @@ int main(int argc, char ** argsv)
   std::ofstream csvFileStream(csvPath.c_str());
 
   // Choose classifiers
-  detector.setDetectSmile(true);
-  detector.setDetectJoy(true);
-  detector.setDetectAllExpressions(true);
-  detector.setDetectAllEmotions(true);
-  detector.setDetectAllEmojis(true);
-  detector.setDetectAllAppearances(true);
+  shared_ptr<PlottingImageListener> listenPtr(new PlottingImageListener(csvFileStream, drawDisplay));
+  detector->setDetectAllExpressions(true);
+  detector->setDetectAllEmotions(true);
+  detector->setDetectAllEmojis(true);
+  detector->setDetectAllAppearances(true);
+  detector->setImageListener(listenPtr.get());
 
   // Initialize detector
-  detector.start();
+  detector->start();
 
   // Process video
-  detector.process(videoPath);
+  bool loop = false;
+  do 
+  {
+    shared_ptr<StatusListener> videoListenPtr = std::make_shared<StatusListener>();
+    detector->setProcessStatusListener(videoListenPtr.get());
 
-  // Stop and reset detector
-  detector.stop();
+    do {
+      if (listenPtr->getDataSize() > 0)
+      {
+        std::pair<Frame, std::map<FaceId, Face>> dataPoint = listenPtr->getData();
+        Frame frame = dataPoint.first;
+        std::map<FaceId, Face> faces = dataPoint.second;
+
+        cerr << "timestamp: " << frame.getTimestamp()
+        << " cfps: " << listenPtr->getCaptureFrameRate()
+        << " pfps: " << listenPtr->getProcessingFrameRate()
+        << " faces: "<< faces.size() << endl;
+
+        listenPtr->outputToFile(faces, frame.getTimestamp());
+      }
+    } while(videoListenPtr->isRunning() || listenPtr->getDataSize() > 0);
+  } while(loop);
+
+  // Stop detector and finish process
+  detector->stop();
+  csvFileStream.close();
+
+  cout << "Output written to file: " << csvPath << endl;
 
   return 0;
 }
